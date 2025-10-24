70e4207fd4a06c18d07575a89e03a01a0c0a9a32
sabilafirdias <sabilafirdias@gmail.com>
2025-10-24 09:51:24 +0700

    membuat card dalam kolom

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -46,6 +46,13 @@ fun AktivitasPertama(modifier: Modifier){
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
-
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ){}
     }
 }
